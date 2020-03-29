94a64a2f530a6ea90e8e8b46b8d73449ee412705
Nikola Puric <nikolapuric1402@gmail.com>
2020-03-29 19:14:13 +0200

    Added SHEETJS library. Enabled importing XLSX file. Added logic for populating table based on the imported XLSX file

--- script.js
+++ script.js
@@ -1,23 +1,90 @@
-// Get list of rows in the table
-var table = document.getElementById("table-main");
-var rows = table.getElementsByTagName("tr");
+const colorMain = "rgb(0, 102, 204) none repeat scroll 0% 0%";
+const colorSucess = "rgb(255, 255, 255) none repeat scroll 0% 0%";
+const colorFail = "rgb(204, 51, 51) none repeat scroll 0% 0%";
 
-const blue = "rgb(0, 102, 204) none repeat scroll 0% 0%";
-const white = "rgb(255, 255, 255) none repeat scroll 0% 0%";
-const red = "rgb(204, 51, 51) none repeat scroll 0% 0%";
+const tableSuccess = document.getElementById("table-success");
+const tableMain = document.getElementById("table-main");
+const tableFail = document.getElementById("table-fail");
+
+function Setup() {
+    GetData();
+    AddRowListeners();
+}
+
+function GetData() {
+
+    var url = "robot_runs_maintenance.xlsx";
+
+    /* set up async GET request */
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "arraybuffer";
+
+    var jsonArray;
+    req.onload = function(e) {
+        var data = new Uint8Array(req.response);
+        var workbook = XLSX.read(data, {type:"array"});
+        var first_sheet_name = workbook.SheetNames[0];
+        var worksheet = workbook.Sheets[first_sheet_name];
+        jsonArray = XLSX.utils.sheet_to_json(worksheet);
+
+        PopulateTable(jsonArray);
+    }
+    req.send();
+}
+
+
+function PopulateTable(jsonArray) {
+    var row;
+    var cell;
+    var i = 0;
+    jsonArray.forEach(element => {
+        i++;
+
+        row = document.createElement("TR");
+
+        cell = document.createElement("TH");
+        cell.setAttribute("scope", "row");
+        cell.setAttribute("class", "col-1");
+        cell.innerHTML = i;
+        row.appendChild(cell);
+
+        cell = document.createElement("TD");
+        cell.setAttribute("class", "col-4");
+        cell.innerHTML = element.RobotName;
+        row.appendChild(cell);
+
+        cell = document.createElement("TD");
+        cell.setAttribute("class", "col-6");
+        cell.innerHTML = element.ScheduleName;
+        row.appendChild(cell);
+        
+        cell = document.createElement("TD");
+        cell.setAttribute("class", "exec-day col-1");
+        cell.innerHTML = element.ExecutionDay;
+        row.appendChild(cell);
+
+        row.style.display = "none";
+
+        tableMain.appendChild(row);
+    });
+
+    ListAvatars();
+}
+
 
 // When we select row it will change background color based on current color
 // Calls function to move row according to current background color
 async function SelectRow(row) {  
 
     if (row.style.background.includes("rgb(0, 102, 204)")  || row.style.background == "") {
-        row.style.background = white;
+        row.style.background = colorSucess;
         row.style.color = "#000";
     } else if (row.style.background.includes("rgb(255, 255, 255)")) {
-        row.style.background = red;
+        row.style.background = colorFail;
         row.style.color = "#fff";
     } else if (row.style.background.includes("rgb(204, 51, 51)")) {
-        row.style.background = blue;
+        row.style.background = colorMain;
         row.style.color = "#fff";
     }
 
@@ -50,14 +117,15 @@ function ListAvatars(day) {
     } else {
         var date = new Date();
         d = date.getDay();
+        if (d===0) {
+            d = 7;
+        }
     }
-    console.log(d);
     
     var executionDays = document.getElementsByClassName("exec-day");
     var daysStr, daysArr, row;
 
     for (i=0; i < executionDays.length; i++) {
-        // console.log(executionDays[i].innerHTML);
         daysStr = executionDays[i].innerHTML;
         daysArr = daysStr.split(",");
         row = executionDays[i].closest("tr");
@@ -81,8 +149,13 @@ function Sleep(ms) {
  }
 
  // Add listeners to all table rows
-for (var i = 0; i < rows.length; i++) {
-    rows[i].addEventListener("click", function() {
-        SelectRow(this);
-    });
+function AddRowListeners() {
+
+    var rows = tableMain.getElementsByTagName("tr");
+
+    for (var i = 0; i < rows.length; i++) {
+        rows[i].addEventListener("click", function() {
+            SelectRow(this);
+        });
+    }
 }
